c7e4f5333f5135d561899e9bfe8ec21836fd6121
piyushbansal20 <115879708+piyushbansal20@users.noreply.github.com>
2023-01-31 00:50:34 +0530

    Solve reload problem

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const incrementButton = document.querySelector('#increment-button');
 const decrementButton = document.querySelector('#dec-button');
 let percentage = 0;
 
+// Get saved percentage from local storage
+const savedPercentage = localStorage.getItem('count');
+
+// If there is a saved percentage, use it, otherwise use 0
+if (savedPercentage) {
+  percentage = parseInt(savedPercentage);
+}
+
+percentageNumber.innerHTML = percentage;
+circlePath.style.strokeDasharray = `${percentage} 100`;
+
 incrementButton.addEventListener('click', () => {
   percentage += 1;
+  localStorage.setItem('count' , percentage);
   if (percentage > 100) {
     percentage = 100;
   }
@@ -15,6 +27,7 @@ incrementButton.addEventListener('click', () => {
 
 decrementButton.addEventListener('click', () => {
   percentage -= 1;
+  localStorage.setItem('count' , percentage);
   if (percentage < 0) {
     percentage = 0;
   }
